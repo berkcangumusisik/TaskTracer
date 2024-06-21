5e7d584e5d3587edcf20e255b43b588be50d54ae
Berkcan Gümüşışık <75336900+berkcangumusisik@users.noreply.github.com>
2024-06-22 01:10:25 +0300

    Revert "TO-16-implement-user-login"

--- userManagment/user_manager.py
+++ userManagment/user_manager.py
@@ -11,11 +11,3 @@ class UserManager:
 
         self.users.append(User(username, password))
         print("Kullanıcı başarıyla kaydedildi.")
-
-    def login(self,username,password):
-        for user in self.users:
-            if user.username == username and user.password == password:
-                self.logged_in_user = user
-                print("Kullanıcı başarıyla giriş yaptı.")
-            else:
-                print("Geçersiz kullanıcı adı veya şifre.")
