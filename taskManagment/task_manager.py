32acdffdfa706e431fd073b62116a523ff1f57b1
Berkcan Gümüşışık <75336900+berkcangumusisik@users.noreply.github.com>
2024-06-22 21:37:33 +0300

    "Added view_task method to TaskManager and modified register method in UserManager to handle duplicate usernames"

--- taskManagment/task_manager.py
+++ taskManagment/task_manager.py
@@ -14,3 +14,13 @@ class TaskManager:
         task = Task(title, description, user.username)
         self.tasks.append(task)
         print("Görev başarıyla eklendi.")
+
+    def view_task(self):
+        user = self.user_manager.get_logged_in_user()
+        if user is None:
+            print("Hiçbir kullanıcı giriş yapmadı.")
+            return
+        user_task = [t for t in self.task if t.assigned_to == user.username]
+        for task in user_task:
+            print(f"Başlık: {task.title}, Açıklama: {task.description}, Tamamlandı mı: {task.is_completed}")
+            
