dcd583e39e728ceb210aad33989961ff1eb87788
Berkcan Gümüşışık <75336900+berkcangumusisik@users.noreply.github.com>
2024-06-23 12:03:37 +0300

    refactor: Update Task class to use Turkish enum values for priority and category

--- taskManagment/task_manager.py
+++ taskManagment/task_manager.py
@@ -1,30 +1,40 @@
-import datetime
 import json
 import os
-from taskManagment.task import PriorityLevel, Task, TaskCategory
+from task import Task, PriorityLevel, TaskCategory
 
 class TaskManager:
     def __init__(self, user_manager):
         self.tasks = []
         self.user_manager = user_manager
+        self.file_path = "tasks.json"
+        self.load_tasks_from_file()
 
-    def add_task(self, title, description, due_date=None, priority=PriorityLevel.MEDIUM, category=TaskCategory.OTHER, tags=None):
-        user = self.user_manager.user_control()
+    def add_task(self, title, description, due_date=None, priority=PriorityLevel.ORTA, category=TaskCategory.DİĞER, tags=None):
+        user = self.user_manager.get_logged_in_user()
+        if user is None:
+            print("Hiçbir kullanıcı giriş yapmadı.")
+            return
+
         task = Task(title, description, user.username, due_date, priority, category, tags)
         self.tasks.append(task)
+        self.save_tasks_to_file()
         print("Görev başarıyla eklendi.")
 
-    def view_task(self):
-        user = self.user_manager.user_control()
-        user_task = [t for t in self.tasks if t.assigned_to == user.username]
-        for task in user_task:
-                    print(f"Başlık: {task.title}, Açıklama: {task.description}, Tamamlandı mı: {task.is_completed}, Bitiş Tarihi: {task.due_date}, Öncelik: {task.priority}, Kategori: {task.category}, Etiketler: {', '.join(task.tags)}, Oluşturulma Tarihi: {task.creation_date}")
+    def get_user_tasks(self):
+        user = self.user_manager.get_logged_in_user()
+        if user is None:
+            print("Hiçbir kullanıcı giriş yapmadı.")
+            return []
+        return [t for t in self.tasks if t.assigned_to == user.username]
 
+    def view_tasks(self):
+        user_tasks = self.get_user_tasks()
+        for task in user_tasks:
+            print(f"Başlık: {task.title}, Açıklama: {task.description}, Tamamlandı mı: {task.is_completed}, Bitiş Tarihi: {task.due_date}, Öncelik: {task.priority}, Kategori: {task.category}, Etiketler: {', '.join(task.tags)}")
 
     def update_task(self, title, new_description):
-        user = self.user_manager.user_control()
-
-        task = next((t for t in self.tasks if t.assigned_to == user.username and t.title == title), None) #next() nesnenin bir sonraki elemanını döndürür.
+        user_tasks = self.get_user_tasks()
+        task = next((t for t in user_tasks if t.title == title), None)
         if task:
             task.description = new_description
             task.history.append(f"Açıklama güncellendi: {new_description}")
@@ -33,50 +43,37 @@ class TaskManager:
         else:
             print("Görev bulunamadı.")
 
-
     def delete_task(self, title):
-        user = self.user_manager.user_control()
-        task = next((t for t in self.tasks if t.assigned_to == user.username and t.title == title), None)
+        user_tasks = self.get_user_tasks()
+        task = next((t for t in user_tasks if t.title == title), None)
         if task:
-            confirmation = input(f"Görevi silmek istediğinizden emin misiniz '{title}'? (evet/hayır): ")
-            if confirmation.lower() == "evet":
-                self.tasks.remove(task)
-                self.save_tasks_to_file()
-                print("Görev başarıyla silindi.")
-            else:
-                print("Görev silme işlemi iptal edildi.")
+            self.tasks.remove(task)
+            self.save_tasks_to_file()
+            print("Görev başarıyla silindi.")
         else:
             print("Görev bulunamadı.")
 
     def search_tasks(self, search_term):
-        user = self.user_manager.user_control()
-        user_tasks = [t for t in self.tasks if t.assigned_to == user.username and (search_term.lower() in t.title.lower() or search_term.lower() in t.description.lower())]
-        for task in user_tasks:
+        user_tasks = self.get_user_tasks()
+        found_tasks = [t for t in user_tasks if search_term.lower() in t.title.lower() or search_term.lower() in t.description.lower()]
+        for task in found_tasks:
             print(f"Başlık: {task.title}, Açıklama: {task.description}, Tamamlandı mı: {task.is_completed}, Bitiş Tarihi: {task.due_date}, Öncelik: {task.priority}, Kategori: {task.category}, Etiketler: {', '.join(task.tags)}")
 
     def sort_tasks(self, by_priority=False, by_due_date=False):
-        user = self.user_manager.get_logged_in_user()
-        if user is None:
-            print("Hiçbir kullanıcı giriş yapmadı.")
-            return
-
-        user_tasks = [t for t in self.tasks if t.assigned_to == user.username]
+        user_tasks = self.get_user_tasks()
         if by_priority:
-            user_tasks = sorted(user_tasks, key=lambda x: x.priority)
+            sorted_tasks = sorted(user_tasks, key=lambda x: x.priority)
         elif by_due_date:
-            user_tasks = sorted(user_tasks, key=lambda x: x.due_date)
+            sorted_tasks = sorted(user_tasks, key=lambda x: x.due_date)
+        else:
+            sorted_tasks = user_tasks
 
-        for task in user_tasks:
+        for task in sorted_tasks:
             print(f"Başlık: {task.title}, Açıklama: {task.description}, Tamamlandı mı: {task.is_completed}, Bitiş Tarihi: {task.due_date}, Öncelik: {task.priority}, Kategori: {task.category}, Etiketler: {', '.join(task.tags)}")
 
-
     def share_task(self, title, share_with_username):
-        user = self.user_manager.get_logged_in_user()
-        if user is None:
-            print("Hiçbir kullanıcı giriş yapmadı.")
-            return
-
-        task = next((t for t in self.tasks if t.assigned_to == user.username and t.title == title), None)
+        user_tasks = self.get_user_tasks()
+        task = next((t for t in user_tasks if t.title == title), None)
         if task:
             shared_task = Task(task.title, task.description, share_with_username, task.due_date, task.priority, task.category, task.tags)
             self.tasks.append(shared_task)
@@ -86,10 +83,8 @@ class TaskManager:
             print("Görev bulunamadı.")
 
     def add_comment(self, title, comment):
-        user = self.user_manager.user_control()
-
-
-        task = next((t for t in self.tasks if t.assigned_to == user.username and t.title == title), None)
+        user_tasks = self.get_user_tasks()
+        task = next((t for t in user_tasks if t.title == title), None)
         if task:
             task.comments.append(comment)
             task.history.append(f"Yorum eklendi: {comment}")
@@ -99,10 +94,8 @@ class TaskManager:
             print("Görev bulunamadı.")
 
     def complete_task(self, title):
-        user = self.user_manager.user_control()
-
-
-        task = next((t for t in self.tasks if t.assigned_to == user.username and t.title == title), None)
+        user_tasks = self.get_user_tasks()
+        task = next((t for t in user_tasks if t.title == title), None)
         if task:
             task.is_completed = True
             task.history.append("Görev tamamlandı.")
@@ -120,43 +113,3 @@ class TaskManager:
             with open(self.file_path, "r") as file:
                 tasks_data = json.load(file)
                 self.tasks = [Task(**task) for task in tasks_data]
-
-    def notify_tasks(self):
-        user = self.user_manager.user_control()
-        user_tasks = [t for t in self.tasks if t.assigned_to == user.username]
-        for task in user_tasks:
-            if task.due_date.date() == datetime.datetime.now().date() and not task.is_completed:
-                print(f"Bildirim: '{task.title}' görevinin bitiş tarihi bugün.")
-
-    def add_subtask(self, task_title, subtask_title):
-        user = self.user_manager.user_control()
-        task = next((t for t in self.tasks if t.assigned_to == user.username and t.title == task_title), None)
-        if task:
-            subtask = Task(subtask_title, "", user.username, task.due_date, task.priority, task.category, task.tags)
-            task.subtasks.append(subtask)
-            task.history.append(f"Alt görev eklendi: {subtask_title}")
-            self.save_tasks_to_file()
-            print("Alt görev başarıyla eklendi.")
-        else:
-            print("Görev bulunamadı.")
-    def display_task_count(self):
-        user = self.user_manager.user_control()
-        user_tasks = [t for t in self.tasks if t.assigned_to == user.username]
-        print(f"Toplam görev sayısı: {len(user_tasks)}")
-
-    def archive_task(self, title):
-        user = self.user_manager.user_control()
-        task = next((t for t in self.tasks if t.assigned_to == user.username and t.title == title), None)
-        if task:
-            task.is_completed = True
-            task.history.append("Görev arşivlendi.")
-            self.save_tasks_to_file()
-            print("Görev başarıyla arşivlendi.")
-        else:
-            print("Görev bulunamadı.")
-
-    def filter_tasks_by_status(self, is_completed):
-        user = self.user_manager.user_control()
-        user_tasks = [t for t in self.tasks if t.assigned_to == user.username and t.is_completed == is_completed]
-        for task in user_tasks:
-            print(f"Başlık: {task.title}, Açıklama: {task.description}, Tamamlandı mı: {task.is_completed}, Bitiş Tarihi: {task.due_date}, Öncelik: {task.priority}, Etiketler: {', '.join(task.tags)}")
